0dd2fa885902b4e7a86b9e1419454e801152c916
c4ei <c4ei.net@gmail.com>
2023-05-15 19:06:46 +0900

    이미지 변경

---
+++ Form1.Designer.cs
@@ -0,0 +1,361 @@
+﻿namespace imgResize
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txt_log = new TextBox();
+            folderBrowserDialog1 = new FolderBrowserDialog();
+            btn_folder = new Button();
+            txt_folder = new TextBox();
+            btn_change = new Button();
+            lbl_x = new Label();
+            txt_x = new TextBox();
+            txt_y = new TextBox();
+            lbl_y = new Label();
+            label1 = new Label();
+            label2 = new Label();
+            btn_make17 = new Button();
+            lbl_17 = new Label();
+            btn_chgSize = new Button();
+            label3 = new Label();
+            label4 = new Label();
+            txt_y2 = new TextBox();
+            label5 = new Label();
+            txt_x2 = new TextBox();
+            label6 = new Label();
+            label7 = new Label();
+            label9 = new Label();
+            txt_newFileName = new TextBox();
+            btn_NameChange = new Button();
+            label10 = new Label();
+            label11 = new Label();
+            btn_make26 = new Button();
+            SuspendLayout();
+            // 
+            // txt_log
+            // 
+            txt_log.Location = new Point(12, 226);
+            txt_log.Multiline = true;
+            txt_log.Name = "txt_log";
+            txt_log.ScrollBars = ScrollBars.Vertical;
+            txt_log.Size = new Size(776, 267);
+            txt_log.TabIndex = 0;
+            // 
+            // btn_folder
+            // 
+            btn_folder.Location = new Point(713, 18);
+            btn_folder.Name = "btn_folder";
+            btn_folder.Size = new Size(75, 23);
+            btn_folder.TabIndex = 1;
+            btn_folder.Text = "폴더선택";
+            btn_folder.UseVisualStyleBackColor = true;
+            btn_folder.Click += btn_folder_Click;
+            // 
+            // txt_folder
+            // 
+            txt_folder.Location = new Point(12, 18);
+            txt_folder.Name = "txt_folder";
+            txt_folder.Size = new Size(695, 23);
+            txt_folder.TabIndex = 2;
+            txt_folder.Text = "D:\\unity\\design\\imgResize\\sample\\212x237_2\\Death";
+            // 
+            // btn_change
+            // 
+            btn_change.Location = new Point(713, 57);
+            btn_change.Name = "btn_change";
+            btn_change.Size = new Size(75, 23);
+            btn_change.TabIndex = 3;
+            btn_change.Text = "변경";
+            btn_change.UseVisualStyleBackColor = true;
+            btn_change.Click += btn_change_Click;
+            // 
+            // lbl_x
+            // 
+            lbl_x.AutoSize = true;
+            lbl_x.Location = new Point(323, 59);
+            lbl_x.Name = "lbl_x";
+            lbl_x.Size = new Size(55, 15);
+            lbl_x.TabIndex = 4;
+            lbl_x.Text = "변경너비";
+            // 
+            // txt_x
+            // 
+            txt_x.Location = new Point(384, 56);
+            txt_x.Name = "txt_x";
+            txt_x.Size = new Size(48, 23);
+            txt_x.TabIndex = 5;
+            txt_x.Text = "212";
+            // 
+            // txt_y
+            // 
+            txt_y.Location = new Point(574, 56);
+            txt_y.Name = "txt_y";
+            txt_y.Size = new Size(42, 23);
+            txt_y.TabIndex = 7;
+            txt_y.Text = "237";
+            // 
+            // lbl_y
+            // 
+            lbl_y.AutoSize = true;
+            lbl_y.Location = new Point(513, 59);
+            lbl_y.Name = "lbl_y";
+            lbl_y.Size = new Size(55, 15);
+            lbl_y.TabIndex = 6;
+            lbl_y.Text = "변경높이";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(441, 61);
+            label1.Name = "label1";
+            label1.Size = new Size(32, 15);
+            label1.TabIndex = 8;
+            label1.Text = "pixel";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(622, 61);
+            label2.Name = "label2";
+            label2.Size = new Size(32, 15);
+            label2.TabIndex = 9;
+            label2.Text = "pixel";
+            // 
+            // btn_make17
+            // 
+            btn_make17.Location = new Point(433, 95);
+            btn_make17.Name = "btn_make17";
+            btn_make17.Size = new Size(141, 23);
+            btn_make17.TabIndex = 10;
+            btn_make17.Text = "17장만들기(루프)";
+            btn_make17.UseVisualStyleBackColor = true;
+            btn_make17.Click += btn_make17_Click;
+            // 
+            // lbl_17
+            // 
+            lbl_17.AutoSize = true;
+            lbl_17.Location = new Point(198, 99);
+            lbl_17.Name = "lbl_17";
+            lbl_17.Size = new Size(229, 15);
+            lbl_17.TabIndex = 11;
+            lbl_17.Text = "skeleton-Walk_0.png~3 까지 폴더에 준비";
+            // 
+            // btn_chgSize
+            // 
+            btn_chgSize.Location = new Point(674, 132);
+            btn_chgSize.Name = "btn_chgSize";
+            btn_chgSize.Size = new Size(114, 23);
+            btn_chgSize.TabIndex = 12;
+            btn_chgSize.Text = "이미지사이즈변경";
+            btn_chgSize.UseVisualStyleBackColor = true;
+            btn_chgSize.Click += btn_chgSize_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(622, 138);
+            label3.Name = "label3";
+            label3.Size = new Size(32, 15);
+            label3.TabIndex = 18;
+            label3.Text = "pixel";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(441, 138);
+            label4.Name = "label4";
+            label4.Size = new Size(32, 15);
+            label4.TabIndex = 17;
+            label4.Text = "pixel";
+            // 
+            // txt_y2
+            // 
+            txt_y2.Location = new Point(574, 133);
+            txt_y2.Name = "txt_y2";
+            txt_y2.Size = new Size(42, 23);
+            txt_y2.TabIndex = 16;
+            txt_y2.Text = "237";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(513, 136);
+            label5.Name = "label5";
+            label5.Size = new Size(55, 15);
+            label5.TabIndex = 15;
+            label5.Text = "변경높이";
+            // 
+            // txt_x2
+            // 
+            txt_x2.Location = new Point(384, 133);
+            txt_x2.Name = "txt_x2";
+            txt_x2.Size = new Size(48, 23);
+            txt_x2.TabIndex = 14;
+            txt_x2.Text = "212";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(323, 136);
+            label6.Name = "label6";
+            label6.Size = new Size(55, 15);
+            label6.TabIndex = 13;
+            label6.Text = "변경너비";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(12, 181);
+            label7.Name = "label7";
+            label7.Size = new Size(91, 15);
+            label7.TabIndex = 19;
+            label7.Text = "파일명일괄변경";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(321, 181);
+            label9.Name = "label9";
+            label9.Size = new Size(123, 15);
+            label9.TabIndex = 23;
+            label9.Text = "변경될 파일명 앞부분";
+            // 
+            // txt_newFileName
+            // 
+            txt_newFileName.Location = new Point(450, 178);
+            txt_newFileName.Name = "txt_newFileName";
+            txt_newFileName.Size = new Size(149, 23);
+            txt_newFileName.TabIndex = 22;
+            txt_newFileName.Text = "skeleton-Idle_";
+            // 
+            // btn_NameChange
+            // 
+            btn_NameChange.Location = new Point(616, 178);
+            btn_NameChange.Name = "btn_NameChange";
+            btn_NameChange.Size = new Size(174, 23);
+            btn_NameChange.TabIndex = 24;
+            btn_NameChange.Text = "파일명 번호빼고 일괄변경";
+            btn_NameChange.UseVisualStyleBackColor = true;
+            btn_NameChange.Click += btn_NameChange_Click;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(12, 140);
+            label10.Name = "label10";
+            label10.Size = new Size(131, 15);
+            label10.TabIndex = 25;
+            label10.Text = "이미지사이즈 일괄변경";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(12, 99);
+            label11.Name = "label11";
+            label11.Size = new Size(180, 15);
+            label11.TabIndex = 26;
+            label11.Text = "이미지4장을 17장으로 순차복사";
+            // 
+            // btn_make26
+            // 
+            btn_make26.Location = new Point(580, 95);
+            btn_make26.Name = "btn_make26";
+            btn_make26.Size = new Size(208, 23);
+            btn_make26.TabIndex = 27;
+            btn_make26.Text = "26장만들기(death3까지준비)";
+            btn_make26.UseVisualStyleBackColor = true;
+            btn_make26.Click += btn_make26_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 505);
+            Controls.Add(btn_make26);
+            Controls.Add(label11);
+            Controls.Add(label10);
+            Controls.Add(btn_NameChange);
+            Controls.Add(label9);
+            Controls.Add(txt_newFileName);
+            Controls.Add(label7);
+            Controls.Add(label3);
+            Controls.Add(label4);
+            Controls.Add(txt_y2);
+            Controls.Add(label5);
+            Controls.Add(txt_x2);
+            Controls.Add(label6);
+            Controls.Add(btn_chgSize);
+            Controls.Add(lbl_17);
+            Controls.Add(btn_make17);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(txt_y);
+            Controls.Add(lbl_y);
+            Controls.Add(txt_x);
+            Controls.Add(lbl_x);
+            Controls.Add(btn_change);
+            Controls.Add(txt_folder);
+            Controls.Add(btn_folder);
+            Controls.Add(txt_log);
+            Name = "Form1";
+            Text = "이미지일괄변경";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txt_log;
+        private FolderBrowserDialog folderBrowserDialog1;
+        private Button btn_folder;
+        private TextBox txt_folder;
+        private Button btn_change;
+        private Label lbl_x;
+        private TextBox txt_x;
+        private TextBox txt_y;
+        private Label lbl_y;
+        private Label label1;
+        private Label label2;
+        private Button btn_make17;
+        private Label lbl_17;
+        private Button btn_chgSize;
+        private Label label3;
+        private Label label4;
+        private TextBox txt_y2;
+        private Label label5;
+        private TextBox txt_x2;
+        private Label label6;
+        private Label label7;
+        private Label label9;
+        private TextBox txt_newFileName;
+        private Button btn_NameChange;
+        private Label label10;
+        private Label label11;
+        private Button btn_make26;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,177 @@
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Globalization;
+
+namespace imgResize
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_change_Click(object sender, EventArgs e)
+        {
+            string path = txt_folder.Text; // 대상 폴더 경로
+            string[] files = Directory.GetFiles(path, "*.png");
+            string new_path = txt_folder.Text + "\\output";
+            if (Directory.Exists(new_path) == false)
+            {
+                Directory.CreateDirectory(new_path);
+            }
+
+            foreach (string file in files)
+            {
+                using (Bitmap bmp = new Bitmap(file))
+                {
+                    int _x = int.Parse(txt_x.Text);
+                    int _y = int.Parse(txt_y.Text);
+                    int _x_dbl = int.Parse(txt_x.Text) * 2; // 212 x 2 --> 424
+                    int _y_dbl = int.Parse(txt_y.Text) * 2;
+                    int _x_cut = int.Parse(txt_x.Text) / 2;
+                    int _y_cut = int.Parse(txt_y.Text) / 2;
+                    Bitmap resized = new Bitmap(bmp, new Size(_x_dbl, _y_dbl)); // 이미지 확대
+                    Bitmap cropped = resized.Clone(
+                        new Rectangle(_x_cut, _y_cut, _x, _y), resized.PixelFormat); // 이미지 자르기
+                    string output = file.Replace(path, path + "\\output");
+                    txt_log.AppendText("\r\n" + output);
+                    cropped.Save(output, ImageFormat.Png); // PNG로 저장
+                }
+            }
+        }
+
+        private void btn_folder_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                txt_folder.Text = fbd.SelectedPath;
+            }
+        }
+
+        private void btn_make17_Click(object sender, EventArgs e)
+        {
+            string sourceFolder = txt_folder.Text; // 이미지 파일이 저장된 폴더 경로
+            string destinationFolder = sourceFolder + "\\output2"; // 결과 이미지 파일이 저장될 폴더 경로
+            string[] imageFiles = { "skeleton-Walk_0.png", "skeleton-Walk_1.png", "skeleton-Walk_2.png", "skeleton-Walk_3.png" };
+            if (Directory.Exists(destinationFolder) == false)
+            {
+                Directory.CreateDirectory(destinationFolder);
+            }
+            int count = 0;
+            for (int i = 0; i < 17; i++)
+            {
+                string sourceFilePath = Path.Combine(sourceFolder, imageFiles[count]);
+                string destfile = "skeleton-Walk_" + i + ".png";
+                string destinationFilePath = Path.Combine(destinationFolder, destfile);
+                txt_log.AppendText("\r\n" + destfile);
+                File.Copy(sourceFilePath, destinationFilePath, true);
+
+                count++;
+                if (count == 4)
+                {
+                    count = 0;
+                }
+            }
+        }
+
+        private void btn_chgSize_Click(object sender, EventArgs e)
+        {
+            string sourceFolder = txt_folder.Text; // 이미지 파일이 저장된 폴더 경로
+            string destinationFolder = txt_folder.Text + "\\resized";
+            int newWidth = int.Parse(txt_x2.Text); // 변경할 이미지의 가로 크기
+            int newHeight = int.Parse(txt_y2.Text); // 변경할 이미지의 세로 크기
+
+            if (!Directory.Exists(destinationFolder))
+            {
+                Directory.CreateDirectory(destinationFolder);
+            }
+
+            foreach (string filePath in Directory.GetFiles(sourceFolder, "*.png"))
+            {
+                string fileName = Path.GetFileName(filePath);
+                string destinationFilePath = Path.Combine(destinationFolder, fileName);
+                ResizeImage(filePath, destinationFilePath, newWidth, newHeight);
+                txt_log.AppendText("\r\n" + destinationFilePath);
+            }
+        }
+
+        private static void ResizeImage(string sourceFilePath, string destinationFilePath, int newWidth, int newHeight)
+        {
+            using (Image sourceImage = Image.FromFile(sourceFilePath))
+            {
+                using (Bitmap bitmap = new Bitmap(newWidth, newHeight))
+                {
+                    using (Graphics graphics = Graphics.FromImage(bitmap))
+                    {
+                        graphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+                        graphics.DrawImage(sourceImage, 0, 0, newWidth, newHeight);
+                        bitmap.Save(destinationFilePath, System.Drawing.Imaging.ImageFormat.Png);
+                    }
+                }
+            }
+        }
+
+        private void btn_NameChange_Click(object sender, EventArgs e)
+        {
+            string sourceFolder = txt_folder.Text; // 파일이 저장된 폴더 경로
+            string prefix = txt_newFileName.Text;  // 새 파일명의 앞부분
+            string suffix = ".png"; // 새 파일명의 뒷부분
+            string destinationFolder = txt_folder.Text + "\\rename";
+            if (!Directory.Exists(destinationFolder))
+            {
+                Directory.CreateDirectory(destinationFolder);
+            }
+            foreach (string filePath in Directory.GetFiles(sourceFolder, "*.png"))
+            {
+                string fileName = Path.GetFileNameWithoutExtension(filePath);
+                txt_log.AppendText("\r\n" + fileName);
+                int numberStartIndex = fileName.Length - 1;
+                string numberString = fileName.Substring(numberStartIndex);
+                txt_log.AppendText("\r\n" + numberString);
+                int number;
+                if (int.TryParse(numberString, out number))
+                {
+                    string newFileName = prefix + numberString + suffix;
+                    string newFilePath = Path.Combine(destinationFolder, newFileName);
+                    File.Copy(filePath, newFilePath);
+                    txt_log.AppendText("\r\n" + newFilePath);
+                }
+            }
+        }
+
+        private void btn_make26_Click(object sender, EventArgs e)
+        {
+            string sourceFolder = txt_folder.Text; // 이미지 파일이 저장된 폴더 경로
+            string destinationFolder = sourceFolder + "\\output26"; // 결과 이미지 파일이 저장될 폴더 경로
+            string[] imageFiles = { "death0.png", "death1.png", "death2.png", "death3.png" };
+            if (Directory.Exists(destinationFolder) == false)
+            {
+                Directory.CreateDirectory(destinationFolder);
+            }
+            int count = 0;
+            for (int i = 0; i < 17; i++)
+            {
+                switch (i)
+                {
+                    case 0 or 1 or 2 or 3 or 4: count = 0; break;
+                    case 5 or 6 or 7 or 8 or 9: count = 1; break;
+                    case 10 or 11 or 12 or 13: count = 2; break;
+                    case 14 or 15 or 16: count = 3; break;
+                }
+
+                string sourceFilePath = Path.Combine(sourceFolder, imageFiles[count]);
+                string destfile = "skeleton-Walk_" + i + ".png";
+                string destinationFilePath = Path.Combine(destinationFolder, destfile);
+                txt_log.AppendText("\r\n" + destfile);
+                File.Copy(sourceFilePath, destinationFilePath, true);
+            }
+
+        }
+    }
+}
